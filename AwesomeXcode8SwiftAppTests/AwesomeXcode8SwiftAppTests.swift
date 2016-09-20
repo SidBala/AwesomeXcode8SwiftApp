9aba7aa18885cd48d6737aaaeaedddebc455a88c
Sid Bala <Sid.Bala@outlook.com>
2016-09-19 18:36:21 -0700

    Initial Commit

---
+++ AwesomeXcode8SwiftAppTests/AwesomeXcode8SwiftAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  AwesomeXcode8SwiftAppTests.swift
+//  AwesomeXcode8SwiftAppTests
+//
+//  Created by Sid Bala on 9/19/16.
+//  Copyright © 2016 Sid Bala. All rights reserved.
+//
+
+import XCTest
+@testable import AwesomeXcode8SwiftApp
+
+class AwesomeXcode8SwiftAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
